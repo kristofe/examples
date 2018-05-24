8d1d4680949b443482e3f385cd6988ddc793d365
Kristofer Schalchter <kristofe72@gmail.com>
2018-05-24 17:31:33 -0400

    Added parameters to model eval script

--- fast_neural_style/run_eval.sh
+++ fast_neural_style/run_eval.sh
@@ -1,2 +1,10 @@
-#python neural_style/neural_style.py eval  --content-image images/content-images/terrain_input.png --model saved_models/mosaic.pth --output-image images/output-images/terrain_mosaic.png --cuda 1
-python neural_style/neural_style.py eval  --content-image images/content-images/terrain_input.png --model saved_models/052318_cw_1e5_sw_1e10_Epoch2.model.pth --output-image images/output-images/terrain_mosaic.png --cuda 1
+model=$1
+if [$# -eq 0 ]
+  then
+    echo "No arguments supplied.  Using default model"
+    model = "saved_models/epoch_2_Thu_May_24_11:23:13_2018_100000.0_1e+15.model.pth"
+fi
+echo "using model: " + $model
+
+python neural_style/neural_style.py eval  --content-image images/content-images/terrain_input.png --model $model --output-image images/output-images/terrain_mosaic.png --cuda 0
+#python neural_style/neural_style.py eval  --content-image images/content-images/terrain_input.png --model checkpoints/ckpt_epoch_1_batch_id_4000.pth --output-image images/output-images/terrain_mosaic.png --cuda 0
